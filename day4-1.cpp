63d0439f25ad10ea82eb6c036b9e98b2e7579105
nverno <noah.v.peart@gmail.com>
2021-12-04 00:53:02 -0500

    day4

---
+++ day4-1.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Board {
+    vector<vector<int>> b;
+    vector<int> rows, cols;
+    std::unordered_map<int,pair<int,int>> idx;
+    int sum;
+    Board(): sum(0) {
+        rows.resize(5,5);
+        cols.resize(5,5);
+    }
+    bool read(istream& is) {
+        int r = 0;
+        bool ok = true;
+        while (ok && r < 5) {
+            vector<int> row(5);
+            for (int c = 0; ok && c < 5; ++c) {
+                if (!(cin >> row[c])) ok = false;
+                idx[row[c]] = {r, c};
+                sum += row[c];
+            }
+            if (ok) b.push_back(row);
+            ++r;
+        }
+        return ok;
+    }
+    int draw(int x) {
+        if (idx.count(x)) {
+            auto [r, c] = idx[x];
+            rows[r]--;
+            cols[c]--;
+            sum -= x;
+            if (rows[r] == 0 || cols[c] == 0)
+                return sum * x;
+        }
+        return -1;
+    }
+};
+
+int main(int argc, char *argv[]) {
+    string line, num;
+    getline(cin, line);
+    istringstream iss(line);
+
+    vector<int> nums;
+    while (getline(iss, num, ','))
+        nums.push_back(stoi(num));
+
+    vector<Board> boards(120);
+    for (auto& b : boards) b.read(cin);
+
+    for (auto& x : nums) {
+        for (auto& b : boards) {
+            int res = b.draw(x);
+            if (res != -1) {
+                cout << res << endl;
+                return 0;
+            }
+        }
+    }
+    return 0;
+}
---
+++ day4-2.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Board {
+    vector<vector<int>> b;
+    vector<int> rows, cols;
+    std::unordered_map<int,pair<int,int>> idx;
+    int sum;
+    bool done;
+    Board(): sum(0), done(false) {
+        rows.resize(5,5);
+        cols.resize(5,5);
+    }
+    bool read(istream& is) {
+        int r = 0;
+        bool ok = true;
+        while (ok && r < 5) {
+            vector<int> row(5);
+            for (int c = 0; ok && c < 5; ++c) {
+                if (!(cin >> row[c])) ok = false;
+                idx[row[c]] = {r, c};
+                sum += row[c];
+            }
+            if (ok) b.push_back(row);
+            ++r;
+        }
+        return ok;
+    }
+    int draw(int x) {
+        if (!done && idx.count(x)) {
+            auto [r, c] = idx[x];
+            rows[r]--;
+            cols[c]--;
+            sum -= x;
+            if (rows[r] == 0 || cols[c] == 0) {
+                done = true;
+                return sum * x;
+            }
+        }
+        return 0;
+    }
+};
+
+int main(int argc, char *argv[]) {
+    string line, num;
+    getline(cin, line);
+    istringstream iss(line);
+
+    vector<int> nums;
+    while (getline(iss, num, ','))
+        nums.push_back(stoi(num));
+
+    vector<Board> boards(120);
+    for (auto& b : boards) b.read(cin);
+
+    int res = -1;
+    for (auto& x : nums) {
+        for (auto& b : boards) {
+            if (b.done) continue;
+            int v = b.draw(x);
+            if (v) res = v;
+        }
+    }
+    cout << res << endl;
+    return 0;
+}
