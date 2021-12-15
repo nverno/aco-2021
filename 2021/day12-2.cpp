03380163da25068e6ce624791f1c96345eebcb5c
nverno <noah.v.peart@gmail.com>
2021-12-14 19:20:43 -0800

    Day12

--- 2021/day12-2.cpp
+++ 2021/day12-2.cpp
@@ -11,39 +11,33 @@ int main(int argc, char *argv[]) {
     while (getline(cin, line)) {
         split(line, parts, '-');
         string u = parts[0], v = parts[1];
-        g[u].push_back(v);
-        g[v].push_back(u);
+        if (v != "start") g[u].push_back(v);
+        if (u != "start") g[v].push_back(u);
     }
 
-    queue<pair<vector<string>,unordered_set<string>>> q;
-    std::unordered_set<string> st;
-    st.insert("start");
-    q.push({{ "start" }, st });
+    queue<tuple<string,unordered_set<string>,bool>> q;
+    q.push({ "start", { "start" }, false });
 
     int res = 0;
     while (!q.empty()) {
-        auto [p, seen] = q.front(); q.pop();
-        string& u = p.back();
-        if (u == "end") {
-            ++res;
-            continue;
-        }
-        for (auto& v : g[u]) {
-            if (v == "end") ++res;
-            else {
-                if (!seen.count(v) || isupper(v[0])) {
-                    std::unordered_set<string> nxt(seen);
-                    vector<string> pp(p);
-                    pp.push_back(v);
-                    nxt.insert(v);
-                    q.push({ pp, nxt });
-                    
+        auto [u, seen, twice] = q.front(); q.pop();
+        if (u == "end") ++res;
+        else {
+            for (auto& v : g[u]) {
+                if (v == "end") ++res;
+                else {
+                    if (!seen.count(v) || isupper(v[0])) {
+                        std::unordered_set<string> nxt(seen);
+                        nxt.insert(v);
+                        q.push({ v, nxt, twice });
+                    }
+                    else if (twice) continue;
+                    else q.push({ v, seen, true });
                 }
             }
         }
     }
 
     cout << res << endl;
-
     return 0;
 }
